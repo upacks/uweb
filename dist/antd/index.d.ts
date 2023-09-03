12283e7fdd9a20ddd364e6640f4d0d4549734a76
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-09-03 09:20:25 +0000

    deploy: 0610a9dba2100d71c843c659124738c2f80e3fbe

---
+++ dist/antd/index.d.ts
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+import ReactDOM from 'react-dom/client';
+import * as antd from 'antd';
+declare const run: (Main: FC | any, Setting: FC | any, _: any) => void;
+export { React, ReactDOM, antd, run, };
+//# sourceMappingURL=index.d.ts.map
