f7b9cbfbfb8ae45bdf11b9cc98049c8551415a28
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-09-03 09:59:49 +0000

    deploy: b01eb93ebcd8f1223e8b3d2ba8d91210f782f44a

---
+++ dist/react.d.ts
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+import ReactDOM from 'react-dom/client';
+declare const Render: (Main: FC | any, Setting: FC | any, _: any) => void;
+export { React, ReactDOM, Render };
+export * from 'antd';
+//# sourceMappingURL=react.d.ts.map
