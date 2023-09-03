c1f16a608dac93fdead2c9a5bd9490cf39762fe6
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-09-03 08:27:30 +0000

    deploy: 05f2bafb03da9082e091dbeb5f1280861c5de966

---
+++ dist/antd/index.d.ts
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+import ReactDOM from 'react-dom/client';
+import * as antd from 'antd';
+import * as icon from '@ant-design/icons';
+declare const run: (Main: FC | any, Setting: FC | any, _: any) => void;
+export { React, ReactDOM, antd, icon, run };
+//# sourceMappingURL=index.d.ts.map
