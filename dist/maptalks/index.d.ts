df07f73ba5769085fa939a13192bfaf22ee184e3
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-09-03 11:31:10 +0000

    Revert v0.0.24

---
+++ dist/maptalks/index.d.ts
@@ -0,0 +1,17 @@
+import * as maptalks from 'maptalks';
+import * as maptalks_three from 'maptalks.three';
+declare class MapView {
+    map: maptalks.Map | any;
+    scene: any;
+    camera: any;
+    threeLayer: maptalks_three.ThreeLayer;
+    conf: any;
+    constructor(conf: any);
+    animateTo: (p: [number, number], threshold?: number) => void;
+    getFilter: (isDark: boolean) => "invert(100%) hue-rotate(180deg) brightness(95%) contrast(90%)" | "sepia(0) invert(0)";
+    setMode: (isDark: boolean) => any;
+    onUpdate: (cb: any) => any;
+    onReady: (cb: any) => any;
+}
+export { MapView, maptalks, maptalks_three, };
+//# sourceMappingURL=index.d.ts.map
