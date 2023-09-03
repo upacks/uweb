ecc5b134818f032f095764dcc51b5e411a7373d2
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-09-03 07:57:59 +0000

    deploy: a18d5c1c9323a0f6172b0baa5f7da19c10801e90

---
+++ dist/antd/index.d.ts
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import * as antd from 'antd';
+import * as icon from '@ant-design/icons';
+declare const _default: {
+    React: typeof React;
+    ReactDOM: typeof ReactDOM;
+    antd: typeof antd;
+    icon: typeof icon;
+    run: (Main: any, Setting: any, _: any) => void;
+};
+export = _default;
+//# sourceMappingURL=index.d.ts.map
