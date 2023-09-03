d8830cad5acbb67b25422f61d3e7b7ba33be8225
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-09-03 08:07:43 +0000

    deploy: eb2da17e31a36984de1258ff153fc436ea81cdad

---
+++ dist/antd/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom/client';
+import * as antd from 'antd';
+import * as icon from '@ant-design/icons';
+import { Doc, KeyValue } from 'utils/web';
+const { theme, ConfigProvider, Layout, FloatButton, Modal } = antd;
+const { BulbOutlined, BulbFilled, SettingOutlined } = icon;
+const run = (Main, Setting, _) => {
+    const DOM = ReactDOM.createRoot(Doc.getElementById('root'));
+    const Render = () => {
+        const { defaultAlgorithm, darkAlgorithm } = theme;
+        const [isDarkMode, setIsDarkMode] = useState(KeyValue('mode') === 'true');
+        const [open, setOpen] = useState(false);
+        useEffect(() => { KeyValue('mode', isDarkMode.toString()); }, [isDarkMode]);
+        return <ConfigProvider theme={{ algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm }}>
+
+            <Layout style={{ height: '100%', width: '100%' }}>
+                <div style={{ height: '100%', width: '100%', background: isDarkMode ? '#333' : '#fff' }}>
+                    <Layout id="main" style={{ margin: 'auto', height: '100%', maxWidth: _.maxWidth ?? 1280, padding: 0 }}>
+                        <Main isDarkMode={isDarkMode}/>
+                    </Layout>
+                </div>
+            </Layout>
+
+            <Modal title="Settings" centered open={open} onCancel={() => setOpen(false)} footer={null} width={768} destroyOnClose={true}>
+                <Setting isDarkMode={isDarkMode}/>
+            </Modal>
+
+            <FloatButton.Group shape="circle" style={{ right: 24, zIndex: 99 }}>
+                <FloatButton onClick={() => setOpen(true)} icon={<SettingOutlined />}/>
+                <FloatButton onClick={() => setIsDarkMode((previousValue) => !previousValue)} icon={isDarkMode ? <BulbOutlined /> : <BulbFilled />}/>
+            </FloatButton.Group>
+
+        </ConfigProvider>;
+    };
+    DOM.render(<Render />);
+};
+export default {
+    React: React,
+    ReactDOM,
+    antd: antd,
+    icon: icon,
+    run
+};
+//# sourceMappingURL=index.jsx.map
