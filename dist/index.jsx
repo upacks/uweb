ae2f328c835ece52b631c670aa018f1c3d126f38
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-09-17 11:19:20 +0000

    deploy: 6cea5aa7965cad041dd286e08ced6416c4e17075

---
+++ dist/index.jsx
@@ -0,0 +1,66 @@
+import { React, Render, Typography, Row, Col } from './react';
+import { ThreeView } from './three';
+import { MapView } from './maptalks';
+import { Vehicle, Toyota } from './utils';
+import { log, Delay } from 'utils/web';
+const { useState, useEffect, useRef } = React;
+const { Title } = Typography;
+console.log('Toyota', Toyota);
+const ll = [43.67338010130343, 105.49508346330428, 0];
+const en = [0, 0, 0];
+const main = ({ isDarkMode }) => {
+    const v0 = useRef({});
+    const v1 = useRef({});
+    const t0 = useRef({});
+    useEffect(() => {
+        const ready = [];
+        const onReady = (indicate) => {
+            log.success(indicate);
+            ready.push(indicate);
+            if (ready.length === 3) {
+                log.success(`Master is ready`);
+                const vehicle = new Vehicle({
+                    Truck: t0.current,
+                    Maptalks: v0.current,
+                    Three: v1.current,
+                });
+                Delay(() => {
+                    vehicle.update({
+                        map: [ll[0], ll[1], 0],
+                        rotate: [0, 0, 0],
+                        MP: { x: en[0], y: en[1], z: 0 }
+                    });
+                }, 500);
+            }
+        };
+        v0.current = new MapView({
+            containerId: 'render_0',
+            simulate: true,
+            isDarkMode,
+            lat: ll[0],
+            lon: ll[1]
+        });
+        v1.current = new ThreeView({
+            containerId: 'render_1',
+            simulate: true,
+            isDarkMode,
+        });
+        Toyota.then((Truck) => {
+            t0.current = Truck;
+            onReady('Toyota is ready');
+        });
+        v0.current.onReady(() => onReady('MapView is ready'));
+        v1.current.onReady(() => onReady('ThreeView is ready'));
+    }, []);
+    useEffect(() => {
+        v0.current.setMode && v0.current.setMode(isDarkMode);
+        v1.current.setMode && v1.current.setMode(isDarkMode);
+    }, [isDarkMode]);
+    return <Row id="main" style={{ height: '100%' }}>
+        <Col id='render_0' span={12} style={{ height: '100%' }}/>
+        <Col id='render_1' span={12} style={{ height: '100%' }}/>
+    </Row>;
+};
+const setting = ({ isDarkMode }) => <Title>:OMG:{isDarkMode ? 'DARK' : 'LIGHT'}</Title>;
+Render(main, setting, { maxWidth: '100%' });
+//# sourceMappingURL=index.jsx.map
