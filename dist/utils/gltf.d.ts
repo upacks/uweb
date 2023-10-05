ca74fc555fb409ec1b0b3158e9eefb8bb2b41c16
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-10-05 03:16:54 +0000

    deploy: f4eddf505ae3ca2f663e324d8f15c2ad80589510

---
+++ dist/utils/gltf.d.ts
@@ -0,0 +1,53 @@
+import * as THREE from 'three';
+interface iGLTF {
+    MapPivot: THREE.Group;
+    ThreePivot: THREE.Group;
+}
+export declare const Drill: ({ size, x, y, z }: {
+    size: number;
+    x: number;
+    y: number;
+    z: number;
+}) => Promise<iGLTF>;
+export declare const Dozer: ({ size, x, y, z }: {
+    size: number;
+    x: number;
+    y: number;
+    z: number;
+}) => Promise<iGLTF>;
+export declare const Toyota: ({ size, x, y, z }: {
+    size: number;
+    x: number;
+    y: number;
+    z: number;
+}) => Promise<iGLTF>;
+export declare class Vehicle {
+    Maptalks: any;
+    TruckMap: any;
+    isM: boolean;
+    Three: any;
+    TruckThree: any;
+    isT: boolean;
+    callback: any;
+    canvas: any;
+    changeCursor: (coll: any, value: string) => void;
+    constructor({ Truck, Maptalks, Three }: any);
+    on: (cb: (event_name: string, args: any) => any) => void;
+    position: ({ gps, utm, r }: {
+        gps: [number, number, number];
+        utm: [number, number, number];
+        r: [number, number, number];
+    }) => null | undefined;
+    update: ({ MP, map, rotate }: {
+        MP: {
+            x: number;
+            y: number;
+            z: number;
+        };
+        map: [number, number, number];
+        rotate: [number, number, number];
+    }) => boolean;
+    remove: () => void;
+}
+export {};
+//# sourceMappingURL=gltf.d.ts.map
