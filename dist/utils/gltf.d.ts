e1e65e63bac710c8b6f658d9d1760c5f5f0bbac7
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-10-09 06:55:57 +0000

    deploy: e66ebc35c74d2074b1b585303f59501ea17cbf7a

---
+++ dist/utils/gltf.d.ts
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+interface iGLTF {
+    MapPivot: THREE.Group;
+    ThreePivot: THREE.Group;
+}
+export declare const Drill: ({ size, x, y, z }: {
+    size: number;
+    x: number;
+    y: number;
+    z: number;
+}) => Promise<iGLTF>;
+export declare const Dozer: ({ size, x, y, z }: {
+    size: number;
+    x: number;
+    y: number;
+    z: number;
+}) => Promise<iGLTF>;
+export declare const Toyota: ({ size, x, y, z }: {
+    size: number;
+    x: number;
+    y: number;
+    z: number;
+}) => Promise<iGLTF>;
+export declare class Vehicle {
+    Maptalks: any;
+    TruckMap: any;
+    isM: boolean;
+    Three: any;
+    TruckThree: any;
+    isT: boolean;
+    callback: any;
+    canvas: any;
+    changeCursor: (coll: any, value: string) => void;
+    constructor({ Truck, Maptalks, Three }: any);
+    on: (cb: (event_name: string, args: any) => any) => void;
+    update: ({ gps, utm, head }: {
+        gps: [number, number, number];
+        utm: [number, number, number];
+        head: number;
+    }) => null | undefined;
+    remove: () => void;
+}
+export {};
+//# sourceMappingURL=gltf.d.ts.map
