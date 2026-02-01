4b133eab83878fe2642d706004cdae1b0f07bac9
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2026-02-01 08:50:30 +0000

    deploy: 91d3824ab7d61b914fbd4fa2ea46d35eb6f044b4

---
+++ dist/react/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom/client';
+import { theme, ConfigProvider, Layout, FloatButton, Modal } from 'antd';
+import { BulbOutlined, BulbFilled, SettingOutlined } from '@ant-design/icons';
+import { Doc, KeyValue } from 'utils/web';
+const Render = (Main, Setting, _) => {
+    const DOM = ReactDOM.createRoot(Doc.getElementById('root'));
+    const Render = () => {
+        const { defaultAlgorithm, darkAlgorithm } = theme;
+        const [isDarkMode, setIsDarkMode] = useState(KeyValue('mode') === 'true');
+        const [open, setOpen] = useState(false);
+        useEffect(() => { KeyValue('mode', isDarkMode.toString()); }, [isDarkMode]);
+        return <ConfigProvider theme={{ algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm }}>
+
+            <Layout style={{ height: '100%', width: '100%' }}>
+                <div style={{ height: '100%', width: '100%', background: isDarkMode ? '#333' : '#fff' }}>
+                    <Layout id="main" style={{ margin: 'auto', height: '100%', maxWidth: _.maxWidth ?? 1280, padding: 0 }}>
+                        <Main setIsDarkMode={setIsDarkMode} isDarkMode={isDarkMode}/>
+                    </Layout>
+                </div>
+            </Layout>
+
+            {Setting ? (<>
+
+                <Modal title="Settings" centered open={open} onCancel={() => setOpen(false)} footer={null} width={768} destroyOnClose={true}>
+                    <Setting setIsDarkMode={setIsDarkMode} isDarkMode={isDarkMode}/>
+                </Modal>
+
+                <FloatButton.Group shape="circle" style={{ right: 24, zIndex: 99 }}>
+                    <FloatButton onClick={() => setOpen(true)} icon={<SettingOutlined />}/>
+                    <FloatButton onClick={() => setIsDarkMode((previousValue) => !previousValue)} icon={isDarkMode ? <BulbOutlined /> : <BulbFilled />}/>
+                </FloatButton.Group>
+
+            </>) : null}
+
+        </ConfigProvider>;
+    };
+    DOM.render(<Render />);
+};
+export { React, ReactDOM, Render };
+export * from 'antd';
+//# sourceMappingURL=index.jsx.map
